8739f9e2a01725fc2094f2d6dd71d0627c548ec6
robin3773 <me.robin377@gmail.com>
2023-01-16 17:36:25 +0600

    New

---
+++ Week 3/Spi/rtl/shift_reg.v
@@ -0,0 +1,18 @@
+module shift_reg #(parameter BIT_LENGTH = 8)(
+    input clk, 
+    input reset_n, 
+    input shif_en, 
+    input serial_in, 
+    output serial_out
+);
+
+    reg [BIT_LENGTH - 1: 0] Q; 
+    always @(posedge clk or negedge reset_n) begin
+        if(!reset_n)
+            Q <= 'b0; 
+        else 
+            Q <= shif_en? {serial_in, Q[BIT_LENGTH-1 : 1]}: Q; 
+    end
+
+    assign serial_out = Q[0]; 
+endmodule 
